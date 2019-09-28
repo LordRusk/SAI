437e2bd189593b620fd47ce53cbb3ca2686e723d
LordRusk <54194338+LordRusk@users.noreply.github.com>
2019-09-28 04:43:08 +0000

    Add files via upload

--- newlpi.sh
+++ newlpi.sh
@@ -71,7 +71,7 @@ partitiondrive() { \
 	rps=$(dialog --inputbox "How big big do you want your root partition with extension? (i.E 30gb) The lowest you want to go is 5gb for a VERY small harddrive. Anything with over 250gb you should make it 30gb." 10 60 3>&1 1>&2 2>&3 3>&1) || exit
 	hps=$(dialog --inputbox "If you want your home partition to be something other than the rest of the drive (maybe you are duel booting) put it bewlow, if not, leave it blank." 10 60 3>&1 1>&2 2>&3 3>&1)
 
-	echo -e "g\nn\np\n1\n\n+500mb\nn\np\n2\n\n+"$rps"\nn\np\n3\n\n"$hps"\nw" | fdisk /dev/sda
+	echo -e "g\nn\np\n1\n\n+500mb\nn\np\n2\n\n+"$rps"\nn\np\n3\n\n"$hps"\nw" | fdisk /dev/"$drive"
 
 	mkfs.fat -F32 /dev/sda1
 	mkfs.ext4 /dev/sda2
@@ -80,14 +80,18 @@ partitiondrive() { \
 	mount /dev/sda2 /mnt
 	mkdir /mnt/home
 	mount /dev/sda3 /mnt/home
+
+	echo "done"
 }
 
 mirrorlist() {
 	mle=$(dialog --title "Would you like to edit the pacman mirror list?" --yesno "The defualt Arch mirror list can be slow and some mirrors just don't work. If you edit the mirror list (by putting # before mirror links you do not want to use" 10 50)
-	if [[ "$mle" == "yes" ]]; then
-		echo "$mle"
-		fi
-	echo $mle
+	if [[ "$mle" == "Yes" ]]; then
+		echo "it work"
+	elif [[ "$mle" == "yes" ]]; then
+		echo "it work lowcase"
+	fi
+	echo $yay
 }
 
 installbase() {
