38a98d39894b5242b0bf9bc96783224ce91ce1ab
LordRusk <54194338+LordRusk@users.noreply.github.com>
2019-09-28 04:22:50 +0000

    Add files via upload

--- newlpi.sh
+++ newlpi.sh
@@ -6,13 +6,13 @@
 error() { printf "Something went wrong, maybe it was the script, maybe it was you, who knows"; exit;}
 
 prescript() { \
-	PS3='LPI needs install; dialog for menus, vim for text editing, and ranger to debug, before the rest of the script can run. Would you like to install dialog, vim, and ranger or quit LPI?: '
-	options=("Install Dialog" "Quit")
+	PS3='LPI needs install; dialog for menus and vim for text editing, before the rest of the script can run. Would you like to install dialog and vim, or quit LPI?: '
+	options=("Install Dialog, vim" "Quit")
 	select opt in "${options[@]}"
 	do
 		case $opt in
 			"Install Dialog")
-				pacman -Sy --noconfirm dialog vim ranger
+				pacman -Sy --noconfirm dialog vim
 				break
 			;;
 			"Quit")
@@ -30,6 +30,44 @@ welcomemsg() { \
 	}
 
 partitiondrive() { \
+	dialog --title "Partitioning and formating" --yesno "First we need to partition the drive, but first we have to choose the drive and wipe it, it will usually be /dev/sda, but it is still good to check. All the current connected drives will be listed, identify which one you want to install Arch and type out the name. DISCLAIMER: WHATEVER DRIVE YOU CHOISE WILL BE WIPED, ARE YOU SURE YOU WANT TO CONTINUE?" 10 60
+
+	PS3='Choose a drive: '
+	options=("/dev/sda/, /dev/sdb/, /dev/sdc/, /dev/sdd/, /dev/sd0")
+	select opt in "${options[@]}"
+	do
+		case $opt in
+			"/dev/sda/")
+				drive="/dev/sda"
+				break
+			;;
+			"/deb/sdb/")
+				drive="/dev/sdb"
+				break
+			;;
+			"/dev/sdc/")
+				drive="/dev/sdc"
+				break
+			;;
+			"/dev/sdd/")
+				drive="/dev/sdd"
+				break
+			;;
+			"/dev/sd0/")
+				drive="/dev/sd0/"
+				break
+			;;
+			"Exit")
+				echo "User Exited."
+				exit
+				break
+			;;
+			*) echo "invalid option $REPLY";;
+		esac
+	done
+
+	dialog --title "DISCLAIMER" --msgbox "If you are reinstalling using LPI on a partition scheme similar to the one LPI makes, it may ask you if you want to continue with the formatting. If it does ask you, just accept and continue." 17 40
+
 	rps=$(dialog --inputbox "How big big do you want your root partition with extension? (i.E 30gb) The lowest you want to go is 5gb for a VERY small harddrive. Anything with over 250gb you should make it 30gb." 10 60 3>&1 1>&2 2>&3 3>&1) || exit
 	hps=$(dialog --inputbox "If you want your home partition to be something other than the rest of the drive (maybe you are duel booting) put it bewlow, if not, leave it blank." 10 60 3>&1 1>&2 2>&3 3>&1)
 
