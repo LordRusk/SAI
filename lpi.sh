bd540e89f3e567ed6f33e8b1a0f655beca254adb
LordRusk <54194338+LordRusk@users.noreply.github.com>
2019-09-29 02:13:38 +0000

    Add files via upload

--- lpi.sh
+++ lpi.sh
@@ -32,7 +32,8 @@ welcomemsg() { \
 partitiondrive() { \
 	dialog --title "Partitioning and formating" --msgbox "First we need to partition the drive, but before we can do that we have to choose the drive to install Arch on, it will usually be /dev/sda, but it is still good to check. All the current connected drives will be listed, identify which drive you would like to continue with and select it. If you wish to not go through with the wiping, formating, and partitioning of any drive, then choose exit" 12 40
 
-	fdisk -l
+	lsblk
+	echo ""
 
 	PS3='Choose a drive: '
 	options=("/dev/sda/" "/dev/sdb/" "/dev/sdc/" "/dev/sdd/" "/dev/sd0", "Exit")
@@ -93,7 +94,7 @@ install() { \
 	dialog --title "It's Finally Time!!" --msgbox "It's time to install Base Arch, so let LPI do its thing, sit back and relax. Depending on how good your internet is, is how fast the install will be. Ready?" 10 35
 	pacstrap /mnt base base-devel dosfstools exfat-utils efibootmgr os-prober mtools network-manager-applet networkmanager wireless_tools wpa_supplicant grub dialog wget git make vim ranger pulseaudio pulseaudio-alsa pavucontrol xorg-server xorg-xinit xorg-xbacklight xcompmgr xwallpaper sxiv unrar unzip zathura zathura-djvu zathura-pdf-mupdf firefox
 
-	dialog --title "Base Install Finished!!" --msgbox "LPI is done installing the base arch system, its time to start configuring things inside the system like grub, locale, etc." 15 15
+	dialog --title "Base Install Finished!!" --msgbox "LPI is done installing the base arch system, its time to start configuring things inside the system like grub, locale, etc." 10 40
 
 }
 
