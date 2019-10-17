1137fc5583798fe31ebd0b8c5f2cb9a2362adf23
LordRusk <54194338+LordRusk@users.noreply.github.com>
2019-10-17 07:39:38 -0700

    some before school updates

--- nlpi2.sh
+++ nlpi2.sh
@@ -36,6 +36,22 @@ locale() {
 	locale-gen
 }
 
+bootmanager() {
+	echo "LPI automatically installs GRUB as it's boot manager, if you would not like to install grub, "
+	echo "but install a different boot manager outside of LPI, select Exit, if not, continue."
+	xprompt="Continue?"
+	grb=$(xit)
+	if [ grb = "Continue" ]; then
+		mkdir /boot/efi
+		mount "$drive"1 /boot/efi
+		grub-install --target=x86_64-efi --bootloader-id=grub-uefi --recheck
+		mkdir /boot/grub/locale
+		cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+		grub-mkconfig -o /boot/grub/grub.cfg
+	elif [ grb = "skip" ]; then
+		echo "ok"
+	fi
+
 ### THE ACTUAL SCRIPT ###
 
 # Re select the drive
