85817fc53a7f60e71df337809099b7c1ff2feb29
LordRusk <lordruskk@gmail.com>
2019-10-21 22:40:12 +0000

    More virtualbox testing, but for non efi

--- lpi2.sh
+++ lpi2.sh
@@ -28,6 +28,10 @@ welcome() {
 
 formatdrive() {
 	clear
+	echo "Are are installing arch on an non EFI bios or an EFI bios."
+	bs=$(echo "non EFI\nEFI" | slmenu -p "non EFI or EFI")
+
+	clear
 	echo "Please select the drive you would like to install arch on"
 	sdrive=$(lsblk -lp | grep "disk $" | awk '{print $1, "(" $4 ")"}' | slmenu -i -p "Choose a drive")
 	cdrive=$(echo "$sdrive" | awk '{print $1}')
@@ -45,16 +49,26 @@ formatdrive() {
 	xprompt="Are you sure you want to continue?"
 	xit
 
-	dd if=/dev/zero of="$cdrive"  bs=512  count=1
-	echo -e "g\nn\np\n1\n\n+500mb\nn\np\n2\n\n+"$rps"\nn\np\n3\n\n"$hps"\nw" | fdisk "$cdrive"
+	if [ "$bs" = "non EFI" ]; then
+		dd if=/dev/zero of="$cdrive" bs=512 count=1
+		echo -e "m\nn\np\n1\n\n"$rps"\nn\np\n2\n\n"$hps"\nw" | fdisk "$cdrive"
 
+		mkfs.ext4 "$cdrive"1
+		mkfs.ext4 "$cdrive"2
+		mount "$cdrive"1 /mnt
+		mkdir /mnt/home
+		mount "$cdrive"2 /mnt/home
+	elif [ "$bs" = "EFI" ]; then
+		dd if=/dev/zero of="$cdrive"  bs=512  count=1
+		echo -e "g\nn\np\n1\n\n+500mb\nn\np\n2\n\n+"$rps"\nn\np\n3\n\n"$hps"\nw" | fdisk "$cdrive"
 
-	mkfs.fat -F32 "$cdrive"1
-	mkfs.ext4 "$cdrive"2
-	mkfs.ext4 "$cdrive"3
-	mount "$cdrive"2 /mnt
-	mkdir /mnt/home
-	mount "$cdrive"3 /mnt/home
+		mkfs.fat -F32 "$cdrive"1
+		mkfs.ext4 "$cdrive"2
+		mkfs.ext4 "$cdrive"3
+		mount "$cdrive"2 /mnt
+		mkdir /mnt/home
+		mount "$cdrive"3 /mnt/home
+	fi
 }
 
 mirrorlist() {
