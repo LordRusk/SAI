f46313e0a9a1b79b0bf4b2e339173e72b64fdaa4
LordRusk <54194338+LordRusk@users.noreply.github.com>
2019-10-25 17:29:15 -0700

    fixen dem errors

--- lpi3.sh
+++ lpi3.sh
@@ -118,7 +118,7 @@ xorgpackages() {
 	echo "Would you like to install the needed packages for a desktop enviroment / window manager? | Xorg packages"
 	xpack=$(echo "Install\nSkip" | slmenu -p "Would you like to install?")
 	if [ "$xpack" = "Install" ]; then
-		pacman -Sy xorg-server xorg-xinit xclip xorg-xbacklight compton xwallpaper
+		pacman --no-confirm -Sy xorg-server xorg-xinit xclip xorg-xbacklight compton xwallpaper
 	fi
 }
 
